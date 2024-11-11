515d14e96c6a64cd57c7af993fa48031f1dd195e
Lily Pad Plunge <ttalphakappa@gmail.com>
2024-11-11 01:45:00 -0800

    dhshs

--- src/portfolio.js
+++ src/portfolio.js
@@ -651,10 +651,10 @@ description:
  // Contact Page
  const contactPageData = {
   contactSection: {
-    title: "Contact Me",
+    title: "About the Team",
     profile_image_path: "animated_ashutosh.png",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "We are a group of engineers!",
   },
   blogSection: {
     title: "Blogs",
