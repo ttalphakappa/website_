87e3a75a97fa16dbbd8332f20cdabc939677d74c
Lily Pad Plunge <ttalphakappa@gmail.com>
2024-11-11 01:56:30 -0800

    Update OSimg.js

--- src/pages/opensource/OSimg.js
+++ src/pages/opensource/OSimg.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class ExperienceImg extends Component {
+export default class OSimg extends Component {
   render() {
     const theme = this.props.theme;
     return (
