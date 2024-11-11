8643ec8f97407d5c9ffd6b34eae7cc5e641cfcc2
Lily Pad Plunge <ttalphakappa@gmail.com>
2024-11-10 23:32:16 -0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -448,13 +448,13 @@ const certifications = {
   ],
 };
 
-// // Experience Page
-// const experience = {
-//   title: "Experience",
-//   subtitle: "Work, Internship and Volunteership",
-//   description:
-//     "I have worked with many evolving startups as ML and DL Developer, Designer and Software Architect. I have also worked with some well established companies mostly as AI Developer. I love organising events and that is why I am also involved with many opensource communities as a representative.",
-//   header_image_path: "experience.svg",
+ // Experience Page
+ const experience = {
+   title: "Experience",
+   subtitle: "Work, Internship and Volunteership",
+   description:
+     "I have worked with many evolving startups as ML and DL Developer, Designer and Software Architect. I have also worked with some well established companies mostly as AI Developer. I love organising events and that is why I am also involved with many opensource communities as a representative.",
+   header_image_path: "experience.svg",
 //   sections: [
 //     {
 //       title: "Work",
@@ -607,7 +607,7 @@ const certifications = {
 //       ],
 //     },
 //   ],
-// };
+ };
 
 // // Projects Page
 // const projectsHeader = {
