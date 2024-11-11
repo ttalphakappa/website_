388ea9cb0df49ca4f77d153c2cf121a9c89cba93
Lily Pad Plunge <ttalphakappa@gmail.com>
2024-11-11 02:42:21 -0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -651,10 +651,10 @@ description:
  // Contact Page
  const contactPageData = {
   contactSection: {
-    title: "Contact Me",
+    title: "ALPHA KAPPA PLEDGE CLASS",
     profile_image_path: "animated_ashutosh.png",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "We are a group of emerging engineers who are passionate about many different things and love to be creative!",
   },
   blogSection: {
     title: "Blogs",
