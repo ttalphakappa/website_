8216e70d0ed8ea24d79479739c2685c3aa07d186
Lily Pad Plunge <ttalphakappa@gmail.com>
2024-11-12 04:08:03 -0800

    te

--- src/portfolio.js
+++ src/portfolio.js
@@ -470,7 +470,7 @@ const certifications = {
            //duration: "June 2023 - Present",
            //location: "San Jose, CA, USA",
            description:
-             "Brainstormed potential ideas.\nBought materials from Amazon and Home Depot.\nCreated CAD designs for all parts",
+             "Brainstormed potential ideas. Bought materials from Amazon and Home Depot. Created CAD designs for all parts",
            color: "#000000",
          },
          {
@@ -480,16 +480,16 @@ const certifications = {
            logo_path: "legato_logo.png",
            //duration: "June 2020 - Aug 2021",
            //location: "Hyderabad, Telangana",
-           description:"Build skeleton for machine. \n Cut dividers and slits. \n Made pseudo code for game logic and moving.",
+           description:"Build skeleton for machine. Cut dividers and slits. Made pseudo code for game logic and moving.",
            color: "#0879bf",
          },
        ],
      },
      {
-       title: "week 2",
+       title: "Week 2: 10/28 - 11/02",
        experiences: [
          {
-           title: "Machine Learning Intern",
+           title: "10/28 - 11/02",
            company: "TikTok Inc.",
            company_url: "https://www.tiktok.com/en/",
            logo_path: "tiktok_logo.png",
@@ -525,7 +525,7 @@ const certifications = {
        ],
      },
      {
-       title: "week 3",
+       title: "11/03 - 11/07",
        experiences: [
          {
            title: "Google Explore ML Facilitator",
@@ -585,6 +585,67 @@ const certifications = {
          },
        ],
      },
+     {
+      title: "11/08 - 11/13",
+      experiences: [
+        {
+          title: "Google Explore ML Facilitator",
+          company: "Google",
+          company_url: "https://about.google/",
+          logo_path: "google_logo.png",
+          duration: "June 2019 - April 2020",
+          location: "Hyderabad, Telangana",
+          description:
+            "Explore Machine Learning (ML) is a Google-sponsored program for university students to get started with Machine Learning. The curriculum offers 3 tracks of ML Content (Beginner, Intermediate, Advanced) and relies on university student facilitators to train other students on campus and to build opensource projects under this program.",
+          color: "#4285F4",
+        },
+        {
+          title: "Microsoft Student Partner",
+          company: "Microsoft",
+          company_url: "https://www.microsoft.com/",
+          logo_path: "microsoft_logo.png",
+          duration: "Aug 2019 - May 2020",
+          location: "Hyderabad, Telangana",
+          description:
+            "Microsoft Student Partner is a program for university students to lead the awareness and use of Cloud especially Azure tools in the development of their projects and startups. Under this program, I have organised hands on workshops and seminars to teach Cloud Computing concepts to students.",
+          color: "#D83B01",
+        },
+        {
+          title: "Mozilla Campus Captain",
+          company: "Mozilla",
+          company_url: "https://www.mozilla.org/",
+          logo_path: "mozilla_logo.png",
+          duration: "Oct 2019 - May 2020",
+          location: "Kurnool, Andhra Pradesh",
+          description:
+            "My responsibility for this program was to create opensource environment in college and in the city. We have organised multiple hackathons on the problems collected by ordinary people from Kurnool city. We have build opensource community of our own college. The community is available at dsc_iiitdmk on github.",
+          color: "#000000",
+        },
+        {
+          title: "Developer Students Club Member",
+          company: "DSC IIITDM Kurnool",
+          company_url:
+            "https://www.linkedin.com/company/developer-students-club-iiitdm-kurnool",
+          logo_path: "dsc_logo.png",
+          duration: "Jan 2018 - May 2020",
+          location: "Kurnool, Andhra Pradesh",
+          description:
+            "We have well established developer club in college which is directly associated with Google Developers. We have developed many interdisciplinary projects under the membership of this club. We have organised workshops and activities on Android Application Development, Flutter and React JS.",
+          color: "#0C9D58",
+        },
+        {
+          title: "Developer Program Member",
+          company: "Github",
+          company_url: "https://github.com/",
+          logo_path: "github_logo.png",
+          duration: "July 2019 - PRESENT",
+          location: "Work From Home",
+          description:
+            "I am actively contributing to many opensource projects. I have contributed to projects of organisations like Tensorflow, Uber, Facebook, Google, Scikit-learn, Kiwix, Sympy, Python, NVLabs, Fossasia, Netrack, Keras etc. These contributions include bug fixes, feature requests and formulating proper documentation for project.",
+          color: "#181717",
+        },
+      ],
+    },
    ],
  };
 
