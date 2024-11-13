1f14489637af077df1f997819fb450fa472e01d2
Lily Pad Plunge <ttalphakappa@gmail.com>
2024-11-13 01:05:54 -0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -375,6 +375,38 @@ description:
  avatar_image_path: "images/baovi.PNG",
 };
 
+const publicationsHeader = {
+   title: "Publications",
+   description: "Some of my published Articles, Blogs and Research.",
+   avatar_image_path: "projects_image.svg",
+ };
+
+ const publications = {
+   data: [
+     {
+       id: "neuro-symbolic-sudoku-solver",
+       name: "Neuro-Symbolic Sudoku Solver",
+       createdAt: "2023-07-02T00:00:00Z",
+       description: "Paper published in KDD KiML 2023",
+       url: "https://arxiv.org/abs/2307.00653",
+     },
+     {
+       id: "mdp-diffusion",
+       name: "MDP-Diffusion",
+       createdAt: "2023-09-19T00:00:00Z",
+       description: "Blog published in Paperspace",
+       url: "https://blog.paperspace.com/mdp-diffusion/",
+     },
+     {
+       id: "consistency-models",
+       name: "Consistency Models",
+       createdAt: "2023-10-12T00:00:00Z",
+       description: "Blog published in Paperspace",
+       url: "https://blog.paperspace.com/consistency-models/",
+     },
+   ],
+ };
+
 
  // mechanics page
 const contactPageData = {
@@ -413,5 +445,7 @@ const contactPageData = {
  certifications,
  experience,
  projectsHeader,
+ publicationsHeader,
+ publications,
  contactPageData,
 };
