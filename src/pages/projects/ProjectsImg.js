ae12ed6a134daae0a878551b85800f02c0603b65
Lily Pad Plunge <ttalphakappa@gmail.com>
2024-11-12 20:24:29 -0800

    Update ProjectsImg.js

--- src/pages/projects/ProjectsImg.js
+++ src/pages/projects/ProjectsImg.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class ProjectsImg extends Component {
   render() {
-    const theme = this.props.theme; 
+    //const theme = this.props.theme; 
 
     return (
       <img
