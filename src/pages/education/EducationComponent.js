079a9178bc2da95fcb95e7f7130e75a959c70149
Lily Pad Plunge <ttalphakappa@gmail.com>
2024-11-12 23:01:07 -0800

    Update EducationComponent.js

--- src/pages/education/EducationComponent.js
+++ src/pages/education/EducationComponent.js
@@ -32,7 +32,7 @@ class Education extends Component {
                   Software
                 </h1>
                 <h3 className="heading-sub-text" style={{ color: theme.text }}>
-                  hello
+                  dflsdnfldldldl
                 </h3>
                 <CompetitiveSites logos={competitiveSites.competitiveSites} />
               </div>
