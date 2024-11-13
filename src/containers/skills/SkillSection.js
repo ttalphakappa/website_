21503370b35f88aee72dd1de51e71e0c3e4f7937
Lily Pad Plunge <ttalphakappa@gmail.com>
2024-11-13 12:40:11 -0800

    :re:

--- src/containers/skills/SkillSection.js
+++ src/containers/skills/SkillSection.js
@@ -8,7 +8,7 @@ import FullStackImg from "./FullStackImg";
 import CloudInfraImg from "./CloudInfraImg";
 import DesignImg from "./DesignImg";
 
-function GetSkillSvg(props) {
+/*function GetSkillSvg(props) {
   if (props.fileName === "DataScienceImg")
     return <DataScienceImg theme={props.theme} />;
   else if (props.fileName === "FullStackImg")
