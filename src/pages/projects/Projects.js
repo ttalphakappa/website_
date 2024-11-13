ce12c9eea1c7171c2252b6be6bef438e273d61aa
Lily Pad Plunge <ttalphakappa@gmail.com>
2024-11-13 04:29:44 -0800

    kys

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -53,13 +53,7 @@ class Projects extends Component {
             return <GithubRepoCard repo={repo} theme={theme} />;
           })}
         </div>
-        <Button
-          text={"More Projects"}
-          className="project-button"
-          href={greeting.githubProfile}
-          newTab={true}
-          theme={theme}
-        />
+        
 
         <div className="repo-cards-div-main">
           {publications.data.map((pub) => {
@@ -70,6 +64,7 @@ class Projects extends Component {
         <Footer theme={this.props.theme} onToggle={this.props.onToggle} />
         <TopButton theme={this.props.theme} />
       </div>
+
     );
   }
 }
--- src/portfolio.js
+++ src/portfolio.js
@@ -386,27 +386,6 @@ description:
 
  const publications = {
    data: [
-     {
-       id: "neuro-symbolic-sudoku-solver",
-       name: "Neuro-Symbolic Sudoku Solver",
-       createdAt: "2023-07-02T00:00:00Z",
-       description: "Paper published in KDD KiML 2023",
-       url: "https://arxiv.org/abs/2307.00653",
-     },
-     {
-       id: "mdp-diffusion",
-       name: "MDP-Diffusion",
-       createdAt: "2023-09-19T00:00:00Z",
-       description: "Blog published in Paperspace",
-       url: "https://blog.paperspace.com/mdp-diffusion/",
-     },
-     {
-       id: "consistency-models",
-       name: "Consistency Models",
-       createdAt: "2023-10-12T00:00:00Z",
-       description: "Blog published in Paperspace",
-       url: "https://blog.paperspace.com/consistency-models/",
-     },
    ],
  };
  // mechanics page
