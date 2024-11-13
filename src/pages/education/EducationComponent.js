5f68c4fcebf34106b0e804fb887e475c56985fa4
Lily Pad Plunge <ttalphakappa@gmail.com>
2024-11-13 13:53:11 -0800

    Update EducationComponent.js

--- src/pages/education/EducationComponent.js
+++ src/pages/education/EducationComponent.js
@@ -29,10 +29,10 @@ class Education extends Component {
               </div>
               <div className="heading-text-div">
                 <h1 className="heading-text" style={{ color: theme.text }}>
-                  Software
+                  Software & Electrical
                 </h1>
                 <h3 className="heading-sub-text" style={{ color: theme.text }}>
-                  website
+                  About the software and electrical components of the Lily Pad Plunge.
                 </h3>
                 <CompetitiveSites logos={competitiveSites.competitiveSites} />
               </div>
