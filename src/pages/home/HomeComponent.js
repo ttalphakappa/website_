d0318b3cc804b5998b2c76c60a280e0b145f648e
Lily Pad Plunge <ttalphakappa@gmail.com>
2024-11-13 10:54:01 -0800

    Update HomeComponent.js

--- src/pages/home/HomeComponent.js
+++ src/pages/home/HomeComponent.js
@@ -13,7 +13,7 @@ class Home extends Component {
         <Greeting theme={this.props.theme} />
         <Skills theme={this.props.theme} />
         <Footer theme={this.props.theme} />
-        <TopButton theme={this.props.theme} />
+        
       </div>
     );
   }
