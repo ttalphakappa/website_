86ac5791c87f419f5950e71d38c134b1adb7535b
Lily Pad Plunge <ttalphakappa@gmail.com>
2024-11-12 23:49:56 -0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -61,58 +61,58 @@ const skills = {
 };
 
 // Education Page
-const competitiveSites = {
-  competitiveSites: [
-    {
-      siteName: "LeetCode",
-      iconifyClassname: "simple-icons:leetcode",
-      style: {
-        color: "#F79F1B",
-      },
-      profileLink: "https://leetcode.com/layman_brother/",
-    },
-    {
-      siteName: "HackerRank",
-      iconifyClassname: "simple-icons:hackerrank",
-      style: {
-        color: "#2EC866",
-      },
-      profileLink: "https://www.hackerrank.com/layman_brother",
-    },
-    {
-      siteName: "Codechef",
-      iconifyClassname: "simple-icons:codechef",
-      style: {
-        color: "#5B4638",
-      },
-      profileLink: "https://www.codechef.com/users/ashutosh_1919",
-    },
-    {
-      siteName: "Codeforces",
-      iconifyClassname: "simple-icons:codeforces",
-      style: {
-        color: "#1F8ACB",
-      },
-      profileLink: "http://codeforces.com/profile/layman_brother",
-    },
-    {
-      siteName: "Hackerearth",
-      iconifyClassname: "simple-icons:hackerearth",
-      style: {
-        color: "#323754",
-      },
-      profileLink: "https://www.hackerearth.com/@ashutosh391",
-    },
-    {
-      siteName: "Kaggle",
-      iconifyClassname: "simple-icons:kaggle",
-      style: {
-        color: "#20BEFF",
-      },
-      profileLink: "https://www.kaggle.com/laymanbrother",
-    },
-  ],
-};
+// const competitiveSites = {
+//   competitiveSites: [
+//     {
+//       siteName: "LeetCode",
+//       iconifyClassname: "simple-icons:leetcode",
+//       style: {
+//         color: "#F79F1B",
+//       },
+//       profileLink: "https://leetcode.com/layman_brother/",
+//     },
+//     {
+//       siteName: "HackerRank",
+//       iconifyClassname: "simple-icons:hackerrank",
+//       style: {
+//         color: "#2EC866",
+//       },
+//       profileLink: "https://www.hackerrank.com/layman_brother",
+//     },
+//     {
+//       siteName: "Codechef",
+//       iconifyClassname: "simple-icons:codechef",
+//       style: {
+//         color: "#5B4638",
+//       },
+//       profileLink: "https://www.codechef.com/users/ashutosh_1919",
+//     },
+//     {
+//       siteName: "Codeforces",
+//       iconifyClassname: "simple-icons:codeforces",
+//       style: {
+//         color: "#1F8ACB",
+//       },
+//       profileLink: "http://codeforces.com/profile/layman_brother",
+//     },
+//     {
+//       siteName: "Hackerearth",
+//       iconifyClassname: "simple-icons:hackerearth",
+//       style: {
+//         color: "#323754",
+//       },
+//       profileLink: "https://www.hackerearth.com/@ashutosh391",
+//     },
+//     {
+//       siteName: "Kaggle",
+//       iconifyClassname: "simple-icons:kaggle",
+//       style: {
+//         color: "#20BEFF",
+//       },
+//       profileLink: "https://www.kaggle.com/laymanbrother",
+//     },
+//   ],
+// };
 
 const degrees = {
   degrees: [
